841530b19c3dc432d5b756807709947d9c7d786b
Madhu <enometh@net.meer>
2026-02-28 21:47:52 +0530

    fix  "Add iffi type :function-prototype-pointer"
    * src/gen/common/util.lisp: (iffi->cffi-type):
    fix breakage from commit 6855df9
    "Add Add iffi type :function-prototype-pointer"
    which breaks generation on declarations of the form
    
    "
    typedef int (*foo) (void *);
    "
    by incorrectly generating
    ```
    (cffi:defctype test-1::foo :void)
    ```
    instead of
    
    ```
    (cffi:defctype test-1::foo (:pointer :void))
    ```

--- src/gen/common/util.lisp
+++ src/gen/common/util.lisp
@@ -214,7 +214,7 @@
           (:function-prototype (%overtype :void))
           (:function-prototype-pointer (let ((overriden (%overtype :function-prototype-pointer)))
                                          (if (eq overriden :function-prototype-pointer)
-                                             (%overtype :void)
+                                             (list :pointer (%overtype :void))
                                              (let ((result-type type)
                                                    (param-types opts))
                                                (list* overriden
